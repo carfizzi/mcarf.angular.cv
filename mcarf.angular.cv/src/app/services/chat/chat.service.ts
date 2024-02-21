db49de44fd844421fab6bf98e7650e6ab51e90ed
Marco <160401397+carfizzi@users.noreply.github.com>
2024-02-21 12:47:39 +0100

    Corrected environment handling

--- mcarf.angular.cv/src/app/services/chat/chat.service.ts
+++ mcarf.angular.cv/src/app/services/chat/chat.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { ChatResponse } from '../../models/responses/chat-response';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
