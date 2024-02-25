8d2f8b630df86842615c7f1f4bd1f12c7334c2d9
Marco <160401397+carfizzi@users.noreply.github.com>
2024-02-25 14:30:04 +0100

    Deleted cv file from git and added logic for its retrieval from firebase storage;

--- mcarf.angular.cv/src/app/app.config.ts
+++ mcarf.angular.cv/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp, getApp } from '@angular/fire/app';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { environment } from '../environments/environment';
 import { ReCaptchaV3Provider, initializeAppCheck, provideAppCheck } from '@angular/fire/app-check';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,7 +16,8 @@ export const appConfig: ApplicationConfig = {
       provideFunctions(() => getFunctions()),
       provideAppCheck(() => initializeAppCheck(getApp(), {
         provider: new ReCaptchaV3Provider('6LcTTH4pAAAAADSEPowmz5xwy7IRwhwb10mfqViG')
-      }))
+      })),
+      provideStorage(() => getStorage()),
     ]),
     provideRouter(routes)
   ]
